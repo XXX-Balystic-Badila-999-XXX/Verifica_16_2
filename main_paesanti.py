d1eb3ba03adf598c30a2cd4018dbb57635947d18
XXX-Balystic-Badila-999-XXX <119594594+XXX-Balystic-Badila-999-XXX@users.noreply.github.com>
2023-02-16 12:22:43 +0100

    Add files via upload

---
+++ main_paesanti.py
@@ -0,0 +1,8 @@
+import main_todisco
+import main_colombi
+
+def saluto_andrea_paesanti():
+    print("Ciao da Andrea Pesanti!")
+
+main_todisco.saluto_mattia_todisco()
+main_colombi.saluto_marco_colombi()
